e573781e2b75c9bb9e471b5839e77b39b24ee54e
HyFren <hyfrencn@gmail.com>
2026-08-22 16:23:37 +0800

    chore: export all generated files in tsand update version to 0.3.4

--- packages/ts/src/index.ts
+++ packages/ts/src/index.ts
@@ -1,2 +1,8 @@
-// packages/ts/src/index.ts
 export * from "./gen/enlangmemo/sync/v1/sync_pb.js";
+export * from "./gen/enlangmemo/sync/v1/entities_pb.js";
+export * from "./gen/enlangmemo/sync/v1/handshake_pb.js";
+export * from "./gen/enlangmemo/sync/v1/pull_pb.js";
+export * from "./gen/enlangmemo/sync/v1/push_pb.js";
+export * from "./gen/enlangmemo/sync/v1/finish_pb.js";
+export * from "./gen/enlangmemo/sync/v1/upload_all_pb.js";
+export * from "./gen/enlangmemo/sync/v1/cancel_pb.js";
